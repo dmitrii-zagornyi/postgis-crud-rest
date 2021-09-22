f867a35984cecff22e8af1136a9fe2683b49f2a5
dmitrii-zagornyi <dmitry.zagorny@gmail.com>
2021-09-22 22:42:52 +0300

    Fixed Polygon and added tests

--- backend/polygon.py
+++ backend/polygon.py
@@ -1,21 +1,24 @@
-from sqlalchemy import Column, Integer, String
+from geoalchemy2 import functions as geoFunctions
+from geoalchemy2 import Geometry
+from geoalchemy2.elements import WKTElement
+from geoalchemy2.shape import to_shape
+from shapely.ops import transform
+from sqlalchemy import Column, DateTime, JSON, Integer, PrimaryKeyConstraint, Sequence, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import functions as sqlFunctions
 
-from geoalchemy2 import functions as geoFunctions
-from geoalchemy2 import Geometry
-
+import pyproj
 
 Base = declarative_base()
 dbSrid = 4326
 
 
-class GisPolygon(Base):
+class Polygon(Base):
     __tablename__ = 'gis_polygon'
 
     # https://stackoverflow.com/questions/13370317/sqlalchemy-default-datetime
-    _created = Column(DateTime(), server_default=sqlFunctions.now())
-    _update = Column(DateTime(), onupdate=sqlFunctions.now())
+    _created = Column(DateTime(timezone=False), server_default=sqlFunctions.now())
+    _update = Column(DateTime(timezone=False), onupdate=sqlFunctions.now())
     id = Column(Integer(), Sequence(f'{__tablename__}_id_seq'), nullable=False, primary_key=True)
 
     class_id = Column(Integer())
@@ -24,37 +27,55 @@ class GisPolygon(Base):
     geom = Column(Geometry('POLYGON', dbSrid))
     PrimaryKeyConstraint(id, name=f'{__tablename__}_pkey')
 
-    def __init__(self, class_id=None, name=None, props=None, geom=None):
-        self.class_id = class_id
-        self.name = name
-        self.props = props
-        self.geom = geom
+    def __init__(self, data, srid=dbSrid):
+        assert type(data) is dict
 
-        self._srid = dbSrid
+        for key, value in data.items():
+            self._setItem(key, value, srid)
+
         return
 
-    def _getSridTransform(self, srid):
-        return self._srid
+    def _getItem(self, key, srid):
+        assert key is not None
+        assert type(key) is str
+        assert srid is not None
+        assert type(srid) is int
 
-    def __getitem__(self, key):
-        value = getattr(self, self.__table__.columns[key])
+        value = getattr(self, key)
+        if value is not None:
+            if (key == 'geom'):
+                if (srid != dbSrid):
+                    dbCrs = pyproj.CRS(f'EPSG:{dbSrid}')
+                    myCrs = pyproj.CRS(f'EPSG:{srid}')
+                    project = pyproj.Transformer.from_crs(dbCrs, myCrs, always_xy=True).transform
+                    value = transform(project, value)
+        return str(value)
+
+    def _setItem(self, key, value, srid):
+        assert key is not None
+        assert value is not None
+        assert srid is not None
+
         if (key == 'geom'):
-            if (self._srid != dbSrid):
-                value = geoFunctions.ST_Transform(value, self._srid)
-            value = geoFunctions.ST_AsText(value)
+            value = WKTElement(value)
+            if (srid != dbSrid):
+                myCrs = pyproj.CRS(f'EPSG:{srid}')
+                dbCrs = pyproj.CRS(f'EPSG:{dbSrid}')
+                project = pyproj.Transformer.from_crs(myCrs, dbCrs, always_xy=True).transform
+                value = transform(project, value)
+        setattr(self, key, value)
+
         return
 
-    def __setitem__(self, key, value):
-        if (key == 'geom'):
-            value = geoFunctions.ST_GeomFromText(value)
-            if (self._srid != dbSrid):
-                value = geoFunctions.ST_Transform(value, self._srid)
-        setattr(self, self.__table__.columns[key], value)
+    def update(self, data, srid=dbSrid):
+        for key, value in data.items():
+            self._setItem(key, value, srid)
+
         return
 
-    def getSrid(self):
-        return self._srid
+    def read(self, srid=dbSrid):
+        data = {}
+        for column in self.__table__.columns:
+            data[column.name] = self._getItem(column.name, srid)
 
-    def setSrid(self, srid):
-        self._srid = srid
-        return
+        return data
---
+++ backend/tests/test_polygon.py
@@ -0,0 +1,75 @@
+from shapely.geometry import Polygon as sgPolygon
+from nose.tools import assert_equals, nottest
+from polygon import Polygon
+
+
+class test_polygon():
+    sgPolygonPoints = [[0,0],[0,1],[1,1],[1,0],[0,0]]
+
+    def test_creation_empty(self):
+        data = {}
+        polygon = Polygon(data)
+        assert polygon is not None
+
+    def test_creation_simple(self):
+        data = {'name': 'test'}
+        polygon = Polygon(data)
+        assert polygon is not None
+
+    def test_creation_with_geom(self):
+        data = {'name': 'test', 'geom': sgPolygon(self.sgPolygonPoints).wkt}
+        polygon = Polygon(data)
+        assert polygon is not None
+
+    def test_creation_with_geom_srid(self):
+        data = {'name': 'test', 'geom': sgPolygon(self.sgPolygonPoints).wkt}
+        polygon = Polygon(data, srid=4326)
+        assert polygon is not None
+
+    def test_creation_with_srid_without_geom(self):
+        data = {'name': 'test'}
+        polygon = Polygon(data, srid=4326)
+
+    def test_read(self):
+        data = {'name': 'test'}
+        polygon = Polygon(data)
+        readData = polygon.read()
+
+        for key in data.keys():
+            assert data[key] == readData[key]
+
+    def test_read_geom(self):
+        data = {'name': 'test', 'geom': sgPolygon(self.sgPolygonPoints).wkt}
+        polygon = Polygon(data)
+        readData = polygon.read()
+
+        for key in data.keys():
+            assert data[key] == readData[key]
+
+    def test_read_geom_srid(self):
+        data = {'name': 'test', 'geom': sgPolygon(self.sgPolygonPoints).wkt}
+        polygon = Polygon(data, 32644)
+        readData = polygon.read(srid=32644)
+
+        for key in data.keys():
+            assert data[key] == readData[key]
+
+    def test_read_srid_without_geom(self):
+        data = {'name': 'test'}
+        polygon = Polygon(data, 32644)
+        readData = polygon.read(srid=32644)
+
+        for key in data.keys():
+            assert data[key] == readData[key]
+
+    def test_update(self):
+        data_first = {'name': 'test', 'geom': sgPolygon(self.sgPolygonPoints).wkt}
+        polygon = Polygon(data_first)
+        data_second = {'geom': sgPolygon(self.sgPolygonPoints).wkt}
+        polygon.update(data_second)
+        readData = polygon.read()
+
+        data = data_first
+        data.update(data_second)
+        for key in data.keys():
+            assert data[key] == readData[key]
